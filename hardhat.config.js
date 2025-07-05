7ac23dd406414849bd30585fa89dac091643a756
Adrian Serrano <adriansb3105@gmail.com>
2025-07-05 01:43:42 -0600

    Fixing deployment issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    scroll: {
+      url: process.env.SCROLL_RPC,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    // local para hardhat node
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    }
+  }
 };
